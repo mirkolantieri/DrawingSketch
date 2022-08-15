7c32a880e9463eca99c2ff9b3c6ec327f10e3518
mirkolantieri <mirklan@outlook.it>
2022-08-15 21:30:58 +0200

    Setup the 2nd part of the DrawingView

--- app/src/main/java/com/mirko/drawingsketch/DrawingView.kt
+++ app/src/main/java/com/mirko/drawingsketch/DrawingView.kt
@@ -3,6 +3,7 @@ package com.mirko.drawingsketch
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /* Instead of utilising an Activity
@@ -38,6 +39,52 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        //      Draw the canvas
+        if (!mDrawPath!!.isEmpty) {
+            // set the stroke and the paint colour
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+//                when pressing on the screen
+//                set the colour path to the right colour
+                mDrawPath!!.color = colour
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(colour, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
 
     }
 
